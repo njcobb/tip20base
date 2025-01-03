28f2f5d64e3e64cb2aef3dd95a008282cc382128
vincentbuff <vincent.m.buff@gmail.com>
2025-01-03 14:59:09 -0500

    Server.js Port Updates (For constant listening)

--- backend/server.js
+++ backend/server.js
@@ -58,9 +58,9 @@ app.get('/results', (req, res) => {
 });
 
 // Declare port number
-const PORT = 3000;
+const PORT = 80;
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
