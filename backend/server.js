cb400bd08912e27cdcf45174987e38cc8adeed3d
vincentbuff <vincent.m.buff@gmail.com>
2025-01-12 15:10:31 -0500

    IP Address Update

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,10 @@ app.get('/results', (req, res) => {
 // Declare port number
 const PORT = 80;
 
+// Declare AWS IP Address
+AWS_IP_ADDR = "3.135.228.238"
+
 // Start the server
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+    console.log(`Server running on http://${AWS_IP_ADDR}:${PORT}`);
 });
